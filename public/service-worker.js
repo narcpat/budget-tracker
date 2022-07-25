0db525b93803f31b0b36d5abcdcc5d5bdf19d2db
narcpat <narcissearts@gmail.com>
2022-07-24 23:07:12 -0400

    moved the service-worker to Public folder. fixed relative paths

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,7 @@
 const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
-const FILES_TO_CACHE = [
-  "./index.html",
-  "./public/css/styles.css",
-  "./public/js/index.js",
-];
+const FILES_TO_CACHE = ["./index.html", "./css/styles.css", "./js/index.js"];
 
 // Respond with cached resources
 self.addEventListener("fetch", function (e) {
