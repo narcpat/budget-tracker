ca037a4000785482caff9e896c470e6f2aba6935
narcpat <narcissearts@gmail.com>
2022-07-25 09:58:37 -0400

    updated server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/Budget-Tracker";
 
 const app = express();
 
@@ -17,11 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/budget-tracker",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
